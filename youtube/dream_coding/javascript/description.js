717c7a3ac791f532efd57820c66172aeea622749
msminsu <msminsu@naver.com>
2022-01-13 15:19:48 +0900

    setting

---
+++ youtube/dream_coding/javascript/description.js
@@ -0,0 +1,40 @@
+'use strict';
+
+console.log('hello');
+
+/* 호이스팅
+    선언전 을 제일 위로 끌어 올려줌;
+ */
+age = 4;
+var age;
+
+/*
+# constants 상수 
+
+Q. 될수 있으면 상수를 사용!!
+A.   보안 - 해커가 값을 바꿀수도 있음,  thread afety ,  실수 방지
+*/
+
+/*
+# primitive :  number, string, boolean, null, undefind, symbol
+object :  
+function( first-class function: 변수에 할당가능, 인자로 전달가능, 함수로 리턴도 가능.) 
+
+Q. 
+A.  
+*/
+ 
+
+let a = 12n;
+ console.log(typeof a)
+
+ let nothing = null; // 값이 null
+ let  x; // 값이 없음 undefined
+
+ /*
+ #  symbol
+ 
+ Q. 
+ A. 
+ */
+ 
